f36d35a41c1d9d6beb9ff81f7d2d60a6fe41f614
dev <dev.camelot.ansh@gmail.com>
2022-07-20 20:19:48 +0200

    First Commit

---
+++ src/GitDemo2/GitDemo2.java
@@ -0,0 +1,10 @@
+package GitDemo2;
+
+public class GitDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Initail Push to GitHub");
+	}
+
+}
